0a2167a6bf67b60f57e57cd0c8bc99e0dd3b9a5b
Simon <64027803+simonsimme@users.noreply.github.com>
2024-04-24 13:32:57 +0200

    Changes by johanbrynne

--- src/PriorityQueue.java
+++ src/PriorityQueue.java
@@ -102,7 +102,7 @@ public class PriorityQueue<E> {
 	// Sifts a node down.
 	// siftDown(index) fixes the invariant if the element at 'index' may
 	// be greater than its children, but all other elements are correct.
-	private void siftDown(int index) {// Complexity :
+	private void siftDown(int index) {// Complexity : O()
 		E value = map.get(index);
 
 		// Stop when the node is a leaf.
